fc1354fef0a32d6ff721a52b9767ba1c414583dd
Andreas Häferer <ahr@ttDev.local>
2017-02-24 13:37:14 +0100

    Sample completed

--- js/h5generator.js
+++ js/h5generator.js
@@ -25,4 +25,114 @@ SOFTWARE.
 */
 
 
+(function (exports) {
+
+    var buildFunction = function (elem,name,rootfnc,rootname) {
+        var html = elem.html;
+        var code = [];
+        var reg = /\{[^\}]+\}/gi;
+        var lines = html.split(reg); expr = html.match(reg) || [];
+        var pos = 0; var vname; var vars = {};
+        for (var i =0; i < lines.length; i++) {
+            code.push(''+JSON.stringify(lines[i]));
+            if (pos < expr.length) {
+                var match = (/\{([\!]{0,1})([a-z]+)(?:\:([^\}]+)){0,1}\}/gi).exec(expr[pos]);
+                vname = match[2];
+                vars[vname] = match[1] == '!';
+                if (match[3] != undefined) {
+                    elem.def[vname] = match[3];
+                }
+                code.push('(\'\'+'+vname+' )');
+                pos++;
+            };
+        };
+        code = code.join('+\n');
+        var fnc;
+        var check = []; var checknames = []; var varnames = [];
+        for (var i in vars) {
+            if (vars[i]) {
+                varnames.push('!'+i);
+                check.push('(_opt.'+i+' == undefined)');
+                checknames.push(i);
+            } else {
+                varnames.push(i);
+            }
+        };
+        if (check.length > 0) check = ['if ('+check.join(' || ')+') throw new Error(\'Generator '+rootname+'.'+name+' requires: ('+checknames.join(',')+') \');'];
+        var fncc = [];
+        fncc.push('fnc = function '+name+' (aOpt,aDef) {');
+        fncc.push('var _def = (aDef == undefined ? '+JSON.stringify(elem.def)+' : aDef); var _opt = (aOpt == undefined ? {} : aOpt);');
+        fncc.push(check.join(''));
+        for (var i in vars ) {
+            fncc.push('var '+i+';');
+        }
+        for (var i in vars ) {
+            fncc.push(''+i+'= (_opt.'+i+' != undefined ? _opt.'+i+': (_def.'+i+' != undefined ? _def.'+i+' : \'\'));');
+            fncc.push('if ('+i+' instanceof Array) '+i+'='+i+'.join(\'\');');
+        }
+        fncc.push('; return '+code+'}')
+        eval(''+fncc.join(''));
+        console.log('Generator created: generators'+rootname+'.'+name+'({'+varnames.join(',')+'})');
+        rootfnc[name] = fnc;
+        elem.fnc = fnc;
+        for (var i in elem.sub) {
+            buildFunction(elem.sub[i],i,fnc,rootname+'.'+name);
+        };
+    };
+
+    var scanGenerator = function (obj,root) {
+        if (obj.data('generator') != undefined) {
+            // There is an Generator
+            var name = obj.data('generator'); 
+            if (name.length > 0) {
+                obj.removeData('generator'); 
+                obj.removeAttr('data-generator');
+                if (root[name] != undefined) throw new Error('Only one generator definition is allowed per child');
+                root.sub[name] = {html: '', sub: {},def: {}};
+                var val = root.sub[name]; 
+                var html = $('<div/>').append(obj.clone());
+                var list = html.find('*[data-generatorplace]');
+                list.map(function (e) {
+                    $(list[e]).replaceWith($('<!--repl:'+$(list[e]).data('generatorplace')+'-->'));
+                });
+                html.find('*[data-generator]').remove();
+                html.find('.htmlGeneratorsDemo').remove();
+                html = html.html();
+                html = html.replace(/\<\!\-\-repl\:([!a-z]+)\-\-\>/gi,function (e,o) {
+                    return '{'+o+'}';
+                });
+                val.html = html;
+                obj.children().map(function (e,obj) {
+                    scanGenerator($(obj),val);
+                });
+            }
+        } else {
+            obj.children().map(function (e,obj) {
+                scanGenerator($(obj),root);
+            });
+        }
+
+    };
+
+    var main = {
+        init: function () {
+          var data = $('.htmlGenerators');
+          if (data.length == 0) return;
+          var start = {sub: {}};
+          data.map(function (e,obj){
+              scanGenerator($(obj),start);
+          });
+          for (var i in start.sub) {
+              buildFunction(start.sub[i],i,main,'');
+          };
+        }
+    };
+
+    exports.generators = main;
+
+})(window);
+
+$(document).ready(function () {
+    generators.init();
+});
 
